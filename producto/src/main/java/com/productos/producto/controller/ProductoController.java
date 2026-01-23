2c0e87233b242c1ecd006033412f58adb14d47fa
KevinRay666 <18170460@itculiacan.edu.mx>
2026-01-22 17:24:42 -0700

    Se crearon los endpoint de tipo GET

--- producto/src/main/java/com/productos/producto/controller/ProductoController.java
+++ producto/src/main/java/com/productos/producto/controller/ProductoController.java
@@ -1,15 +1,53 @@
 package com.productos.producto.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
+import com.productos.producto.dto.ApiResponse;
+import com.productos.producto.entity.Producto;
+import com.productos.producto.service.ProductoService;
+import com.productos.producto.util.BadResponse;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ProductoController {
 
-    
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("/products")
+    public ResponseEntity<ApiResponse> getAll() {
+
+        List<Producto> list = productoService.findAll();
+        ApiResponse response = new ApiResponse(list);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ApiResponse> getId(@PathVariable Long id) {
+
+        Optional<Producto> producto = productoService.findById(id);
+
+        if (producto.isEmpty()) {
+
+            BadResponse badResponse = new BadResponse("ID_INVALIDO", "Ingrese un Id valido");
+            ApiResponse response = new ApiResponse(badResponse);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+        }
+
+        ApiResponse response = new ApiResponse(producto);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }
--- producto/src/main/java/com/productos/producto/service/ProductoService.java
+++ producto/src/main/java/com/productos/producto/service/ProductoService.java
@@ -1,11 +1,37 @@
 package com.productos.producto.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.productos.producto.entity.Producto;
+import com.productos.producto.repository.IProductoRepository;
 
 
 @Service
 public class ProductoService {
 
+    @Autowired
+    private IProductoRepository productoRepository;
 
+
+        public List<Producto>findAll(){
+        List<Producto> productos = productoRepository.findAll();
+        List<Producto> productosEnexisteList = new ArrayList<>();
+
+        for (Producto producto : productos) {
+            if (producto.getCantidad() > 0) {
+                productosEnexisteList.add(producto);
+            }
+        }
+        return productosEnexisteList;
+    }
+
+       public Optional<Producto> findById(Long id){
+            return productoRepository.findById(id);
+    }
     
 }
